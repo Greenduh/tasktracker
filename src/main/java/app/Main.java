b4b8fb986456b8137ba9c240d49ec769eea1f0ea
nikita <greendsmain@gmail.com>
2025-09-13 00:27:00 +0300

    add "add" function

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,21 +1,54 @@
 package app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        // interrupt execution if no arguments are passed
+        if (args.length == 0) {
+            System.out.println("No operation specified.");
+            return;
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
         File tasksFile = new File(System.getProperty("user.home") + "/tasks.json");
 
         // create tasks.json if it doesn't exist
-        if (!tasksFile.exists()) {
-            try {
-                tasksFile.createNewFile();
+        try {
+            if (tasksFile.createNewFile()) {
                 System.out.println("File created: " + tasksFile.getName());
-            } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
             }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        switch (args[0]) {
+            case "add":
+                Task task = new Task(args[1]);
+                try {
+                    mapper.writeValue(tasksFile, task);
+                    System.out.printf("Task added successfully (ID: %d)\n", task.getId());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case "update":
+                break;
+            case "delete":
+                break;
+            case "mark":
+                break;
+            case "list":
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + args[0]);
         }
     }
 }
