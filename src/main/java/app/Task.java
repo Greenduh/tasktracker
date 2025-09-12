dc4d5cb9dbd5ff7eba6707d2481cbfb0dce1228f
nikita <greendsmain@gmail.com>
2025-09-12 22:05:43 +0300

    init

---
+++ src/main/java/app/Task.java
@@ -0,0 +1,48 @@
+package app;
+
+import java.time.LocalDateTime;
+
+public class Task {
+
+    private int id;
+    private String description;
+    private TaskStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task(String description) {
+        this.id = 1; // placeholder
+        this.description = description;
+        this.status = TaskStatus.TODO;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
---
+++ src/main/java/app/TaskStatus.java
@@ -0,0 +1,7 @@
+package app;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    DONE;
+}
